6f903badb0128f8b21b30246d1974209c156f24d
ilovetiktiok <alexdubovyck@yandex.ru>
2023-04-04 13:37:46 +0300

    В буфер копировать

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-import datetime
+import datetime, pyperclip
 
 def convert_from(link):
-    # text = 'https://youtu.be/Gmt3Nk9KoMc?t=370'
-
-    # Извлечение времени из текста
-    # - вызывается функция `split('=')`, которая разделяет строку "https://youtu.be/Gmt3Nk9KoMc?t=370" на массив подстрок, разделенных знаком "=": ["https://youtu.be/Gmt3Nk9KoMc?t", "370"].
-    # time_str = link.split('=')[1].split('t=')[-1]
     time_str = link.split('=')[1]
-
-    # Преобразование строки в число
     time_sec = int(time_str)
-
-    # Вычисление времени
     time = str(datetime.timedelta(seconds=time_sec))
-
-    # Вывод времени
-    print(f"Время: {time}")
-
+    result = time + " - "
+    pyperclip.copy(result)
+    print(result)
 
 
 
 try:
     print('Нажми: Копировать URL видео с привязкой по времени и нажми Enter')
     x = input()
-    # print("x", x)
     convert_from(x)
 except Exception as e:
     print("Произошла ошибка: ", e)
